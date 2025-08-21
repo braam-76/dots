0d5f147db234dca9f4d706cdb949e7ac37d1e01e
braam-76 <braam76.gamidov@yandex.com>
2025-08-21 20:00:16 +0300

    Changed some files

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,9 +2,8 @@ return {
   {
     "mason-org/mason.nvim",
     config = function()
-      require("mason").setup({
-      })
-    end
+      require("mason").setup({})
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
@@ -14,31 +13,33 @@ return {
         "lua_ls",
         "ts_ls",
         "clangd",
-        "gopls"
-      }
+        "gopls",
+      },
     },
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "williamboman/mason.nvim",           -- For installing LSP servers
+      "williamboman/mason.nvim",        -- For installing LSP servers
       "williamboman/mason-lspconfig.nvim", -- For configuring LSP servers
     },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
+        capabilities = capabilities,
         settings = {
           Lua = {
             diagnostics = {
-              globals = { 'vim' },
+              globals = { "vim" },
             },
             format = {
               enable = true,
               -- Put format options here
               -- NOTE: the value should be String!
               defaultConfig = {
-                indent_style = 'space',
-                indent_size = '2',
+                indent_style = "space",
+                indent_size = "2",
               },
             },
             workspace = {
@@ -48,15 +49,27 @@ return {
           },
         },
       })
-      lspconfig.ts_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.gopls.setup({})
-      vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover,
-        { desc = "[l]sp: get info on [h]over (nvim-lspconfig)" })
-      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition,
-        { desc = "[l]sp: goto [d]efinition (nvim-lspconfig)" })
-      vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action,
-        { desc = "[l]sp: code [a]ction (nvim-lspconfig)" })
-    end
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      vim.keymap.set(
+        "n",
+        "<leader>lh",
+        vim.lsp.buf.hover,
+        { desc = "[l]sp: get info on [h]over (nvim-lspconfig)" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>ld",
+        vim.lsp.buf.definition,
+        { desc = "[l]sp: goto [d]efinition (nvim-lspconfig)" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>la",
+        vim.lsp.buf.code_action,
+        { desc = "[l]sp: code [a]ction (nvim-lspconfig)" }
+      )
+    end,
   },
 }
