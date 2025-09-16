de1010c72197dc27d19283ec6e116bbafc5b1f17
braam-76 <braam76.gamidov@yandex.com>
2025-09-16 18:19:58 +0300

    Changed mason to blink.nvim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,45 +1,46 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "rafamadriz/friendly-snippets", },
     version = "1.*",
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+      },
 
       appearance = {
         nerd_font_variant = "mono",
       },
 
+
       -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+      completion = {
+        documentation = {
+          auto_show = false
+        },
       },
+      {
+        'neovim/nvim-lspconfig',
+        dependencies = { 'saghen/blink.cmp' },
 
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'saghen/blink.cmp' },
-
-    -- example using `opts` for defining servers
-    opts = {
-      servers = {
-        lua_ls = {},
-        clangd = {},
-        zls = {},
+        -- example using `opts` for defining servers
+        opts = {
+          servers = {
+            lua_ls = {},
+            clangd = {},
+            zls = {},
+          }
+        },
+        config = function(_, opts)
+          local lspconfig = require('lspconfig')
+          
+          for server, config in pairs(opts.servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+          end
+        end
       }
-    },
-    config = function(_, opts)
-      local lspconfig = require('lspconfig')
-      for server, config in pairs(opts.servers) do
-        -- passing config.capabilities to blink.cmp merges with the capabilities in your
-        -- `opts[server].capabilities, if you've defined it
-        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end
+    }
   }
 }
