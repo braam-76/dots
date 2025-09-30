e39dc52e797b2642e51f40be1f5d60ff99b16acb
Ilgar Gamidov <braam@debian>
2025-09-30 13:31:32 +0300

    Minor config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,11 @@
-function fish_greeting
-	screenfetch
-end
+# function fish_greeting
+# 	screenfetch
+# end
 
 set -U fish_user_paths $HOME/.local/share/pnpm $fish_user_paths
 
 zoxide init fish | source
 starship init fish | source
+
+# Created by `pipx` on 2025-09-25 18:17:07
+set PATH $PATH /home/braam/.local/bin
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,9 +4,33 @@ config.default_prog = { "/usr/bin/fish", "-l" }
 
 config.font = wezterm.font "ZedMono Nerd Font"
 config.font_size = 12
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Novel"
+
+config.animation_fps = 60   -- smoothen easing effects for cursor blink and visual bell
+config.cursor_blink_ease_in = "Linear"
+config.cursor_blink_ease_out = "Linear"
 
 -- config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'modus-vivendi'
+  else
+    return 'modus-operandi'
+  end
+end
+
+wezterm.on('window-config-reloaded',
+  function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    local appearance = window:get_appearance()
+    local scheme = scheme_for_appearance(appearance)
+    local colors = wezterm.get_builtin_color_schemes()[scheme]
+    if overrides.color_scheme ~= scheme then
+      overrides.color_scheme = scheme
+      window:set_config_overrides(overrides)
+    end
+  end)
 
 return config
