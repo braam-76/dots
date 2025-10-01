e4ce5dd302ff98fd4ea51c23318229894918d3ec
Ilgar Gamidov <braam@debian>
2025-10-01 21:34:05 +0300

    Neovide and conform config

---
+++ .config/nvim/lua/braam/plugins/conform.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+  { src = "https://github.com/stevearc/conform.nvim" },
+})
+
+local conform = require("conform")
+
+conform.setup({
+  formatters_by_ft = {
+    python = { "ruff_organize_imports", "ruff_format" },
+    lua = { "stylua" },
+  },
+})
+
+vim.keymap.set("n", "<leader>lf", function()
+  conform.format()
+end)
---
+++ .config/nvim/lua/braam/plugins/neovide.lua
@@ -0,0 +1,5 @@
+if vim.g.neovide then
+  vim.o.guifont = "ZedMono Nerd Font:h14"
+  vim.o.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+end
