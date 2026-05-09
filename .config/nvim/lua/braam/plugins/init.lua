ac01f9d61aee118a3574010858cf44d1c9aee1ba
braam-76 <braam76.gamidov@yandex.com>
2026-05-09 15:21:25 +0300

    Removed lot of files

--- .config/nvim/lua/braam/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-require "braam/plugins/colorscheme"
-require "braam/plugins/flash"
-require "braam/plugins/neogit"
-require "braam/plugins/mini"
-require "braam/plugins/oil"
--- most chashable part (because I change the most)
-require "braam/plugins/lspconfig"
-require "braam/plugins/conform"
--- require "braam/plugins/yazi"
