4f162e1e9839c6748aa74a02dc5c5bfaa25a018a
braam-76 <braam76.gamidov@yandex.com>
2026-05-18 15:45:24 +0300

    Disable language provider support (lua and vimscript plugins only)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,13 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
+-- disable language provider support (lua and vimscript plugins only)
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
+
 vim.o.clipboard = "unnamedplus"
 
 vim.o.relativenumber = true
