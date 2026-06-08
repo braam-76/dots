c1c1990b7c8e204db1581d28f5f585e7843efe8f
Ilgar Gamidov <braam76.gamidov@yandex.com>
2026-06-08 18:15:56 +0300

    Moved BACK to vim.pack

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.keymap.set
-
-map("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-map("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-map("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-map("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-map("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
